5db20ef89e2c9240e991cd4da185dd8f9a30e22f
4rham <arhamalaghny@gmail.com>
2026-07-22 15:13:48 +0700

    Update db_kuliah.sql to remove CREATE DATABASE statements

---
+++ db_kuliah.sql
@@ -0,0 +1,14 @@
+
+CREATE TABLE mahasiswa (
+    nim VARCHAR(15) PRIMARY KEY,
+    nama VARCHAR(50),
+    asal VARCHAR(30)
+);
+
+INSERT INTO mahasiswa (nim, nama, asal) VALUES
+('18.83.1233', 'Salsabila', 'Bantul'),
+('18.83.1234', 'Rahmadi', 'Sleman'),
+('18.83.1235', 'Sukarwo', 'Kulon Progo'),
+('18.83.1236', 'Pambudi', 'Kulon Progo'),
+('18.83.1237', 'Putri Amalia', 'Yogyakarta'),
+('24.83.1125', 'Arham', 'Surabaya');
